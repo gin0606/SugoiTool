0b727ccb7b0e2faceb8b0cd1e882b61cb34d924d
gin0606 <kkgn06@gmail.com>
2016-02-12 16:30:32 +0900

    Add SugoiToolKit target with unit test

---
+++ SugoiToolKit/SugoiToolKit.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SugoiToolKit.
+FOUNDATION_EXPORT double SugoiToolKitVersionNumber;
+
+//! Project version string for SugoiToolKit.
+FOUNDATION_EXPORT const unsigned char SugoiToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SugoiToolKit/PublicHeader.h>
+
+
